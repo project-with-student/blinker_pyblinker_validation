66a454b5cb91891a0f0ef9183631c0391c2e5c5a
rpb <balandongiv@gmail.com>
2025-09-24 08:34:08 +0800

    Add pyproject.toml and setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyblinker',
+      version='0.0.1',
+      description='An open-source alternative to MATLAB BLINKER, integrated with MNE-Python, supporting multimodal biosignal research with flexible workflows, reproducible metrics, and enhanced accessibility.',
+      author='rpb',
+      packages=['src'],
+      install_requires=['seaborn','mne']
+      )
